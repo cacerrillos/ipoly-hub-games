defa222fb5093987dfe28eaa298f6a71139f405d
Carlos Cerrillos <carlos@cerrillostech.com>
2017-05-10 15:15:36 -0700

    lambda dump from project-stable

---
+++ lambda/APIHandler/RegistrationRequests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StableAPIHandler {
+	public struct PresentationStats {
+		public uint count;
+		public uint max;
+		public PresentationStats(uint count, uint max) {
+			this.count = count;
+			this.max = max;
+			full = count >= max;
+		}
+		public bool full;
+	}
+	public class RegistrationRequest {
+		public uint viewer_id {
+			get;
+			set;
+		}
+		public string viewer_key {
+			get;
+			set;
+		}
+		public uint date {
+			get;
+			set;
+		}
+		public uint block_id {
+			get;
+			set;
+		}
+		public uint presentation_id {
+			get;
+			set;
+		}
+	}
+	public class RegistrationResponse {
+		public bool status;
+		public List<ProjectStableLibrary.Registration> data;
+		public List<ProjectStableLibrary.Schedule> full;
+		public ViewerSavedError error = null;
+	}
+	public class ViewerSavedError {
+		public uint code = 0;
+		public string message = "";
+	}
+}
---
+++ lambda/APIHandler/SignupRequest.cs
@@ -0,0 +1,75 @@
+
+using System.Collections.Generic;
+
+namespace StableAPIHandler {
+	public class SignupRequest {
+		public string first_name {
+			get;
+			set;
+		}
+		public string last_name {
+			get;
+			set;
+		}
+		public uint house {
+			get;
+			set;
+		}
+		public uint grade {
+			get;
+			set;
+		}
+	}
+	public class SignupResponse : ProjectStableLibrary.Viewer {
+		public SignupResponse(ProjectStableLibrary.Viewer v) {
+			viewer_id = v.viewer_id;
+			viewer_key = v.viewer_key;
+			first_name = v.first_name;
+			last_name = v.last_name;
+			house_id = v.house_id;
+			grade_id = v.grade_id;
+		}
+		public bool status {
+			get;
+			set;
+		}
+	}
+	public class SignupErrorResponse {
+		public SignupErrorResponse() {
+			status = false;
+		}
+		public SignupErrorResponse(int code) {
+			this.status = false;
+			this.code = code;
+		}
+		public bool status {
+			get;
+			set;
+		}
+		public int code {
+			get;
+			set;
+		}
+		public string Message;
+		public List<ProjectStableLibrary.Registration> data;
+		public List<ProjectStableLibrary.Schedule> full;
+	}
+	public class FinishSignupRequest {
+		public uint viewer_id {
+			get;
+			set;
+		}
+		public string viewer_key {
+			get;
+			set;
+		}
+		public List<uint> data {
+			get;
+			set;
+		}
+		public bool status {
+			get;
+			set;
+		}
+	}
+}
