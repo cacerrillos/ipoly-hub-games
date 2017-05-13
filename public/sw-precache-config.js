56ca6970c755778440ba2f472cb836f2d863b203
Carlos Cerrillos <carlos@cerrillostech.com>
2017-05-12 22:34:36 -0700

    sw-config and relative api url

---
+++ public/sw-precache-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  staticFileGlobs: [
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+  ],
+  navigateFallback: '',
+  runtimeCaching: [
+  ],
+  verbose: true,
+  debug: true
+};
